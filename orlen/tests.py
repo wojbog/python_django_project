a3eac181e7ace6882fb51f7877c676136b6db9e2
wojbog <wojtek44wojciech@gmail.com>
2022-06-13 14:13:03 +0200

    test models

--- orlen/tests.py
+++ orlen/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import PetrolPump, Vehicle
+
+
+class TestModel(TestCase):
+
+    def test_model_PetrolPump(self):
+        obj = PetrolPump.objects.create(name="test", size=5.0)
+        self.assertEqual(str(obj), "test")
+
+    def test_model_Vehicle(self):
+        obj = Vehicle.objects.create(name="test", max_size_tank=5.0)
+        self.assertEqual(str(obj), "test")
